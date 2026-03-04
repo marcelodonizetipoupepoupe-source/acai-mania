ce76d9d324d0ac028017f9b5e49794a9513fe233
marcelodonizetipoupepoupe-source <marcelodonizetipoupepoupe@gmail.com>
2026-03-04 14:21:07 -0300

    Update criar-pix.js

--- api/criar-pix.js
+++ api/criar-pix.js
@@ -1,41 +1,35 @@
+import mercadopago from "mercadopago";
+
+mercadopago.configure({
+  access_token: process.env.MP_ACCESS_TOKEN
+});
+
 export default async function handler(req, res) {
 
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Método não permitido" });
   }
 
-  const TOKEN = process.env.MP_ACCESS_TOKEN;
-
-  if (!TOKEN) {
-    return res.status(500).json({ error: "Token não configurado" });
-  }
-
   try {
 
     const { total } = req.body;
 
-    const response = await fetch(
-      "https://api.mercadopago.com/v1/payments",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${TOKEN}`,
-        },
-        body: JSON.stringify({
-          transaction_amount: Number(total),
-          description: "Pedido Açaí",
-          payment_method_id: "pix",
-          payer: { email: "cliente@email.com" }
-        }),
+    const payment = await mercadopago.payment.create({
+      transaction_amount: Number(total),
+      description: "Pedido Açaí",
+      payment_method_id: "pix",
+      payer: {
+        email: "teste@teste.com"
       }
-    );
+    });
 
-    const data = await response.json();
-
-    return res.status(200).json(data);
+    res.status(200).json({
+      qr_code: payment.body.point_of_interaction.transaction_data.qr_code,
+      qr_code_base64: payment.body.point_of_interaction.transaction_data.qr_code_base64
+    });
 
   } catch (error) {
-    return res.status(500).json({ error: "Erro ao gerar PIX" });
+    console.error(error);
+    res.status(500).json({ error: "Erro ao gerar PIX" });
   }
 }
